3b7018b49c4b1bf2fa578a9cf4930df972d35f7a
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-22 11:23:53 +0100

    build up the fetchapi for deleting entries

--- account/js/entries.js
+++ account/js/entries.js
@@ -23,10 +23,20 @@ const fetchAllEntries = (token) => {
                                     <p>${entryContent}</p>
                                     <small>${dateTime}</small>
                                 </td>
-                                <td><a href="#" id='${entryId}'><i class="far fa-trash-alt"></i></a></td>
+                                <td><a class="deleteBtn" href="#${entryId}" id='${entryId}'><i class="far fa-trash-alt"></i></a></td>
                              </tr>`;
             }
             entriesBody.innerHTML = table_rows;
+            var anchors = document.getElementsByClassName("deleteBtn");
+            for (var i = 0, length = anchors.length; i < length; i++) {
+              var anchor = anchors[i];
+              anchor.addEventListener('click', function(e) {
+                  e.preventDefault();  
+                  console.log(this.getAttribute('id'));
+                  const entryId = this.getAttribute('id');
+                  deleteEntry(entryId);
+              }, true);
+            };
         }else{
             document.getElementById('body').innerHTML = "UNAUTHOIZED ACCESS";
         }
@@ -36,4 +46,25 @@ const fetchAllEntries = (token) => {
         // regText.innerHTML = 'Login';
     });
     };
+    const deleteEntry = (entryId) => {
+        const token = localStorage.getItem('token');
+        console.log(token); 
+        console.log(entryId);
+        const url = `https://nelson-diary.herokuapp.com/api/v1/entries/${entryId}`;
+        // const url = `http://localhost:8000/api/v1/entries/${entryId}`;
+        fetch(url, { method: 'DELETE', headers: { 'x-access-token':token,'Content-Type': 'application/json; charset=utf-8'  } })
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+            if(data.success){
+                console.log('success');
+                fetchAllEntries(token);
+            }else{
+                console.log('not success');
+            }
+        }).catch( (err) => {
+            console.log('Request failed', err);
+        }); 
+    }
     fetchAllEntries(token);
